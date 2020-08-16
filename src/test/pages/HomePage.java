42797685165d35982eb7cf30243386e64fee0c84
srmazur <63271426+srmazur@users.noreply.github.com>
2020-08-16 11:44:46 -0400

    Verify Vesability

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -1,10 +1,15 @@
 package pages;
 
 import base.BasePage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomePage extends BasePage {
     protected WebDriver driver;
@@ -27,9 +32,16 @@ public class HomePage extends BasePage {
     @FindBy(id = "adminPass")
     public WebElement passwordField;
 
-    @FindBy(id = "idname")
-    public WebElement signIn_Btn;
+    @FindBy(css = "a.navbar-brand.ml-3")
+    public List<WebElement> list_of_elements;
 
     @FindBy(linkText = "Synchronization")
     public WebElement navBtn_synchronization;
-}
+
+
+
+
+
+        }
+
+
--- src/test/tests/HomePageTest.java
+++ src/test/tests/HomePageTest.java
@@ -4,10 +4,12 @@ import base.BaseTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.HomePage;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Method;
 
 public class HomePageTest extends BaseTest {
@@ -20,8 +22,16 @@ public class HomePageTest extends BaseTest {
     }
 
 
-    @Test(testName = "Verify Title", description = "This test validates title of Home page", groups = "smoke")
+    @Test(testName = "Verify Title", description = "This test validates title of Home page")
     public void verifyTitle() {
         Assert.assertEquals(getDriver().getTitle(), "TLA Automation");
     }
-}
+    @Test(testName = "Verify Vesability" , description = "This test validating vesability")
+    public void VerefVes(){
+        for (int i = 0 ; i <homePage.list_of_elements.size();i++ ){
+            Assert.assertTrue(homePage.list_of_elements.get(i).isDisplayed());
+
+        }
+
+    }
+    }
