bd0107747f888e0b359073c04b77f7018434de7e
srmazur <63271426+srmazur@users.noreply.github.com>
2020-08-16 10:49:34 -0400

    Verify Title

--- src/test/pages/HomePage.java
+++ src/test/pages/HomePage.java
@@ -1,35 +1,35 @@
-//package pages;
-//
-//import base.BasePage;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.support.FindBy;
-//import org.openqa.selenium.support.PageFactory;
-//
-//public class HomePage extends BasePage {
-//    protected WebDriver driver;
-//
-//    public HomePage(WebDriver driver) {
-//        super(driver);
-//        this.driver = driver;
-//        PageFactory.initElements(driver, this);
-//    }
-//
-//    @FindBy(xpath = "//title")
-//    public WebElement title;
-//
-//    @FindBy(id = "title")
-//    public WebElement mainHeader;
-//
-//    @FindBy(id = "admin")
-//    public WebElement usernameField;
-//
-//    @FindBy(id = "adminPass")
-//    public WebElement passwordField;
-//
-//    @FindBy(id = "idname")
-//    public WebElement signIn_Btn;
-//
-//    @FindBy(linkText = "Synchronization")
-//    public WebElement navBtn_synchronization;
-//}
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+    protected WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(xpath = "//title")
+    public WebElement title;
+
+    @FindBy(id = "title")
+    public WebElement mainHeader;
+
+    @FindBy(id = "admin")
+    public WebElement usernameField;
+
+    @FindBy(id = "adminPass")
+    public WebElement passwordField;
+
+    @FindBy(id = "idname")
+    public WebElement signIn_Btn;
+
+    @FindBy(linkText = "Synchronization")
+    public WebElement navBtn_synchronization;
+}
--- src/test/tests/HomePageTest.java
+++ src/test/tests/HomePageTest.java
@@ -10,25 +10,18 @@ import pages.HomePage;
 
 import java.lang.reflect.Method;
 
-//public class HomePageTest extends BaseTest {
-//    HomePage homePage;
-//
-//    @BeforeMethod(alwaysRun = true)
-//    public void setUp(ITestResult result, Method method) {
-//        super.setUp(method, result);
-//        homePage = new HomePage(getDriver());
-//    }
-//
-//
-//    @Test(description = "Testing header of the page")
-//    public void verifyMainHeaderText() {
-//        extentTest.log(LogStatus.INFO, "Captured title of the page: " + homePage.mainHeader.getText());
-//        Assert.assertEquals(homePage.mainHeader.getText(), "Automation with Selenium");
-//        screenshot.takeScreenshotAndLog();
-//    }
-//
-//    @Test(testName = "Verify Title", description = "This test validates title of Home page", groups = "smoke")
-//    public void verifyTitle() {
-//        Assert.assertEquals(getDriver().getTitle(), "TLA Automation");
-//    }
-//}
+public class HomePageTest extends BaseTest {
+    HomePage homePage;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp(ITestResult result, Method method) {
+        super.setUp(method, result);
+        homePage = new HomePage(getDriver());
+    }
+
+
+    @Test(testName = "Verify Title", description = "This test validates title of Home page", groups = "smoke")
+    public void verifyTitle() {
+        Assert.assertEquals(getDriver().getTitle(), "TLA Automation");
+    }
+}
